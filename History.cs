331e2ab7f2ea2f4ff7fb7e0f72e4e89e13a05604
SuchGlasha <such.glasha@gmail.com>
2015-11-13 00:37:21 +0300

    Create History.cs
    Класс история

---
+++ History.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace CashMana.Models
+{
+    public class History
+    {
+        public double Amount { get; set; }
+        public bool Income { get; set; }
+        public DateTimeOffset DateOfOperation { get; set; }
+        public Category CurrentCategory { get; set; }
+        public int Idhis { get; set; }
+
+        public History()
+        {
+            Amount = 0;
+            Income = false;
+            DateOfOperation = DateTime.Today;
+            CurrentCategory = new Category();
+        }
+    }
+}
