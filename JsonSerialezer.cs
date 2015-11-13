293c866ad71a39c6ccc2090a6984234a23e43b3c
dariavladimirovna <dulya-1803@mail.ru>
2015-11-14 02:27:58 +0300

    Начало работы с данными

---
+++ JsonSerialezer.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CashMana.Models;
+using CashMana.Models.Categories;
+using Newtonsoft.Json;
+
+namespace CashMana.JsonSerializer
+{
+    public class JsonSerilizer
+    {
+        
+    }
+}
