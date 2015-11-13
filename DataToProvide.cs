ea995aa74f2b0727773f9c09cc692acbe22e38a4
dariavladimirovna <dulya-1803@mail.ru>
2015-11-14 02:31:31 +0300

    Сохранение данных

---
+++ DataToProvide.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CashMana.Models;
+
+namespace CashMana
+{
+    public class DataToProvide
+    {
+        public int id { get; set; }
+        public Profile MyProfile { get; set; }
+
+    }
+}
